36d402f4457456d6ada3a845f66c9a7ca458c844
Laura <laurareyeslarrosa@gmail.com>
2026-07-06 11:53:02 -0300

    Set projects component

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
+  standalone: true,
   imports: [],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-  projects = ['Project 1', 'Project 2'];
 }
