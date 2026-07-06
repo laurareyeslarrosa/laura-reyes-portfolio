6267bc5640ea290198aec90e55fc28172a29bb79
Laura <laurareyeslarrosa@gmail.com>
2026-07-06 11:53:09 -0300

    Set skills component

--- src/app/features/skills/skills.component.ts
+++ src/app/features/skills/skills.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
+  standalone: true,
   imports: [],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-  skills = ['Angular', 'React', 'CSS', 'UX'];
 }
