cdf300e11f5c0c826088889fd1940882d8a0e4dc
Laura <laurareyeslarrosa@gmail.com>
2026-07-21 17:41:58 -0300

    Improve language service

--- src/app/core/services/i18n.service.ts
+++ src/app/core/services/i18n.service.ts
@@ -1,17 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import en from '../../../assets/i18n/en.json';
 import es from '../../../assets/i18n/es.json';
 
 @Injectable({ providedIn: 'root' })
 export class I18nService {
-  private lang: 'en' | 'es' = 'en';
+  private key = 'language';
   private translations = { en, es };
+  private lang = signal<'es' | 'en'>(this.getLang());
 
   setLang(lang: 'en' | 'es') {
-    this.lang = lang;
+    localStorage.setItem(this.key, lang);
+    this.lang.set(lang);
+  }
+
+  getLang(): 'en' | 'es' {
+    return (localStorage.getItem(this.key) as 'en' | 'es') || 'es';
   }
 
   t(key: string): string {
-    return key.split('.').reduce((obj: any, k: string) => obj?.[k], this.translations[this.lang]) || key;
+    return (
+      key
+        .split('.')
+        .reduce(
+          (obj: any, k: string) => obj?.[k],
+          this.translations[this.lang()],
+        ) || key
+    );
   }
 }
