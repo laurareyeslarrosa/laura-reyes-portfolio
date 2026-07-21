54ee69f0be1ef302185246dd33de5dd83469ab87
Laura <laurareyeslarrosa@gmail.com>
2026-07-21 17:43:44 -0300

    delete footer call from layout

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -2,20 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
-    selector: 'app-page',
-    standalone: true,
-    imports: [
-        HeaderComponent,
-        FooterComponent,
-        RouterOutlet
-    ],
-    templateUrl: './layout.component.html',
-    styleUrls: ['./layout.component.scss']
+  selector: 'app-page',
+  standalone: true,
+  imports: [HeaderComponent, RouterOutlet],
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
 })
-
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
